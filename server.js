53857ab5f0d7b365ff517bb73a6b3ffdfe084ea6
Ahsan Riaz <malikahsansultan@gmail.com>
2025-05-13 13:09:44 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sql = require('mssql');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true,
+        trustServerCertificate: false
+    }
+};
+
+app.post('/register', async (req, res) => {
+    const { name, email, phone } = req.body;
+
+    try {
+        await sql.connect(dbConfig);
+        await sql.query`INSERT INTO Users (Name, Email, Phone) VALUES (${name}, ${email}, ${phone})`;
+        res.send('Registration Successful!');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error saving data');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
